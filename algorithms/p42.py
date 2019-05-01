7052ecdc315253d48aab823180fc96796baf13c6
shapled <shapled@shapled.com>
2019-05-01 11:32:51 +0800

    resolve problem 42 (42 -> 41)

--- algorithms/p42.py
+++ algorithms/p42.py
@@ -4,13 +4,37 @@ from typing import List
 
 
 class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        return min(set(range(1, len(nums)+2)) - set(nums))
+    def trap(self, height: List[int]) -> int:
+        """ 按照括号匹配相同的思路做就行了 """
+        ret = 0
+        changed = True
+        while changed:
+            changed = False
+            i = 1
+            left = -1
+            while i < len(height):
+                if height[i-1] > height[i]:
+                    left = i - 1
+                elif height[i-1] == height[i]:
+                    pass
+                else:  # <
+                    if left != -1:
+                        to = min(height[left], height[i])
+                        for j in range(left+1, i):
+                            ret += to - height[j]
+                            height[j] = to
+                        if to == height[left]:
+                            left = -1
+                        changed = True
+                i += 1
+        return ret
 
 
 if __name__ == '__main__':
     s = Solution()
-    assert s.firstMissingPositive([1, 2, 0]) == 3
-    assert s.firstMissingPositive([3, 4, -1, 1]) == 2
-    assert s.firstMissingPositive([7, 8, 9, 11, 12]) == 1
-    assert s.firstMissingPositive([1, 2, 3]) == 4
+    assert s.trap([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
+    assert s.trap([]) == 0
+    assert s.trap([0, 0, 0]) == 0
+    assert s.trap([0, 6, 2, 3, 4]) == 3
+    assert s.trap([1, 1, 1]) == 0
+    assert s.trap([3, 1, 1, 5, 0, 4, 8, 1, 1]) == 10
